a59c898924485b2df09ba4a98d9236a37af62590
salaros <salaros@yandex.ru>
2017-09-19 00:11:27 +0300

    bumping the version to 1.1.5-beta

--- src/net45/Properties/AssemblyInfo.cs
+++ src/net45/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ClassLibrary1")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("CodeCave.Revit.Toolkit")]
+[assembly: AssemblyDescription("A toolkit for processing .rfa, .rvt etc without Revit")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ClassLibrary1")]
+[assembly: AssemblyCompany("CodeCave, PUE")]
+[assembly: AssemblyProduct("Revit.Toolkit")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.5.*")]
+[assembly: NeutralResourcesLanguage("en")]
+
